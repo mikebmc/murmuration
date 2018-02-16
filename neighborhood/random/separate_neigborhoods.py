f651ef866a7e41442c897e0bc17eadd0517807c9
King Crawford <crawboy@me.com>
2018-02-16 09:27:03 -0500

    add changes to separate_neigborhoods.py before rm

--- neighborhood/random/separate_neigborhoods.py
+++ neighborhood/random/separate_neigborhoods.py
@@ -17,7 +17,7 @@ def mkcsv(chunk, fname):
 
 
 data_dir = '../../data/'
-read = os.path.join(data_dir, 'june17.csv')
+read = os.path.join(data_dir, 'may17.csv')
 
 num_hoods = 265
 for hood in range(1, num_hoods + 1):
@@ -25,19 +25,17 @@ for hood in range(1, num_hoods + 1):
         '../neighborhood' + str(hood), 'data.csv')
     cleanup(write)
 
-
 label = 'tpep_pickup_datetime'
 chunksize = 10 ** 6
 for chunk in pd.read_csv(read, chunksize=chunksize):
-    chunk['count'] = 1
     chunk.index = pd.to_datetime(chunk[label])
     chunk.drop(label, axis=1, inplace=True)
-    to_save = chunk.groupby('PULocationID').resample('30T').sum()
+    to_save = chunk.groupby('PULocationID').resample('60T').size().fillna(0)
 
-    print('to_save.head() =\n{}'.format(to_save.head()))
-    quit()
-    write = os.path.join(
-        '../neighborhood' + str(hood), 'data.csv')
-    tosave = chunk.loc[chunk.loc[:, 'PULocationID'] == hood]
-    tosave = tosave.resample('30T').sum()
-    mkcsv(tosave, write)
+    print('to_save.head(10) =\n{}'.format(to_save.head(10)))
+    print('type(to_save) =', type(to_save))
+
+    to_save = to_save.reset_index().groupby('PULocationID')
+    for num, group in to_save:
+        write = os.path.join('../neighborhood' + str(num), 'data.csv')
+        mkcsv(group, write)
